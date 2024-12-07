9b1dc0fa80fddf3ac7b211e3ecf5c1d205e54974
Jonathan Kumarich <jonathankumarich@gmail.com>
2024-12-07 09:05:51 +0200

    feat: day 07 part 01

---
+++ src/aoc/day_07.py
@@ -0,0 +1,31 @@
+from typing import TextIO
+from itertools import product
+
+
+def part_01(input: TextIO) -> int:
+    tests = [line.split(": ") for line in input.readlines()]
+    tests = [(int(test[0]), list(map(int, test[1].split()))) for test in tests]
+
+    total_correct = 0
+    for res, eq in tests:
+        configurations = product("*+", repeat=len(eq) - 1)
+        for configuration in configurations:
+            total = eq[0]
+            for n, num in enumerate(eq[1:]):
+                match configuration[n]:
+                    case "*":
+                        total *= num
+                    case "+":
+                        total += num
+                    case _:
+                        raise ValueError
+
+
+            if total == res:
+                total_correct += res
+                break
+
+    return total_correct
+
+def part_02(input: TextIO) -> int:
+    pass
