b48b9461a8d44bc6e83c149109545b82753cf07e
Jonathan Kumarich <jonathankumarich@gmail.com>
2024-12-07 09:06:04 +0200

    feat: day 07 part 02 bruteforce

--- src/aoc/day_07.py
+++ src/aoc/day_07.py
@@ -27,5 +27,29 @@ def part_01(input: TextIO) -> int:
 
     return total_correct
 
+
 def part_02(input: TextIO) -> int:
-    pass
+    tests = [line.split(": ") for line in input.readlines()]
+    tests = [(int(test[0]), list(map(int, test[1].split()))) for test in tests]
+
+    total_correct = 0
+    for n, (res, eq) in enumerate(tests):
+        configurations = product("*+|", repeat=len(eq) - 1)
+        for configuration in configurations:
+            total = eq[0]
+            for n, num in enumerate(eq[1:]):
+                match configuration[n]:
+                    case "*":
+                        total *= num
+                    case "+":
+                        total += num
+                    case "|":
+                        total = int(str(total) + str(num))
+                    case _:
+                        raise ValueError
+
+            if total == res:
+                total_correct += res
+                break
+
+    return total_correct
