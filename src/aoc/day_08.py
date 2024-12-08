143feab5c895b2d692bb07b0414a8e9061abdac0
Jonathan Kumarich <jonathankumarich@gmail.com>
2024-12-08 20:03:12 +0100

    feat: day 08 part 02

--- src/aoc/day_08.py
+++ src/aoc/day_08.py
@@ -1,24 +1,27 @@
 from collections import defaultdict
-from copy import deepcopy
 from itertools import combinations
 from typing import TextIO
 
 
-def part_01(input: TextIO) -> int:
+def antenna_locations(grid: list[list[str]]) -> dict[str, set[tuple[int, int]]]:
     antennas = defaultdict(set)
-    lines = [list(line.strip()) for line in input.readlines()]
-    width = len(lines[0])
-    height = len(lines)
-
-    for y, line in enumerate(lines):
-        for x, char in enumerate(line):
-            if char == '.':
+    for x, line in enumerate(grid):
+        for y, char in enumerate(line):
+            if char == ".":
                 continue
             antennas[char].add((x, y))
 
+    return antennas
+
+
+def part_01(input: TextIO) -> int:
+    lines = [list(line.strip()) for line in input.readlines()]
+    width = len(lines[0])
+    height = len(lines)
+    antennas = antenna_locations(lines)
+
     antinodes = set()
-    viz = defaultdict(set)
-    for key, locations in antennas.items():
+    for locations in antennas.values():
         pairs = list(combinations(locations, 2))
 
         for a, b in pairs:
@@ -29,24 +32,43 @@ def part_01(input: TextIO) -> int:
 
             if 0 <= antinode_a[0] < height and 0 <= antinode_a[1] < width:
                 antinodes.add(antinode_a)
-                viz[key].add(antinode_a)
 
             if 0 <= antinode_b[0] < height and 0 <= antinode_b[1] < width:
                 antinodes.add(antinode_b)
-                viz[key].add(antinode_b)
-
-    # for key, values in viz.items():
-    #     print('\n key: ', key)
-    #     grid = deepcopy(lines)
-    #     for (x, y) in values:
-    #         grid[y][x] = '#'
-    #
-    #     for line in grid:
-    #         print(''.join(line))
-
 
     return len(antinodes)
 
 
 def part_02(input: TextIO) -> int:
-    pass
+    lines = [list(line.strip()) for line in input.readlines()]
+    width = len(lines[0])
+    height = len(lines)
+    antennas = antenna_locations(lines)
+    antinodes = set()
+
+    for locations in antennas.values():
+        antinodes.union(locations)
+        pairs = list(combinations(locations, 2))
+
+        for a, b in pairs:
+            distance = (b[0] - a[0], b[1] - a[1])
+
+            # a ->
+            n = 0
+            while True:
+                antinode = (a[0] - (n * distance[0]), a[1] - (n * distance[1]))
+                if not (0 <= antinode[0] < height and 0 <= antinode[1] < width):
+                    break
+                antinodes.add(antinode)
+                n += 1
+
+            # b ->
+            n = 0
+            while True:
+                antinode = (b[0] + (n * distance[0]), b[1] + (n * distance[1]))
+                if not (0 <= antinode[0] < height and 0 <= antinode[1] < width):
+                    break
+                antinodes.add(antinode)
+                n += 1
+
+    return len(antinodes)
