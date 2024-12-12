f99e3abf45c04c365251e3a21918bea4338460c1
Jonathan Kumarich <jonathankumarich@gmail.com>
2024-12-12 16:45:26 +1300

    feat: day 11 part 02

--- src/aoc/day_11.py
+++ src/aoc/day_11.py
@@ -1,3 +1,4 @@
+from collections import Counter, defaultdict
 from functools import lru_cache
 from typing import TextIO
 
@@ -11,15 +12,14 @@ def part_01(input: TextIO) -> int:
 
     return total
 
+
 @lru_cache
 def blink_stone(stone: int, n_times: int) -> int:
-    print(stone, n_times)
     if n_times == 0:
         return 1
 
     if stone == 0:
         return blink_stone(1, n_times - 1)
-
     elif (stone_len := len(str(stone))) % 2 == 0:
         return blink_stone(
             int(str(stone)[: int(stone_len / 2)]), n_times - 1
@@ -28,11 +28,27 @@ def blink_stone(stone: int, n_times: int) -> int:
         return blink_stone(stone * 2024, n_times - 1)
 
 
+
 def part_02(input: TextIO) -> int:
-    stones = map(int, input.read().strip().split())
+    stones = Counter(map(int, input.read().strip().split()))
 
-    total = 0
-    for stone in stones:
-        total += blink_stone(stone, 25)
+    for _ in range(75):
+        new = defaultdict(lambda: 0)
+        for stone, n in stones.items():
+            for s in blink_once(stone):
+                new[s] += n
 
-    return total
+        stones = new
+
+    return sum(stones.values())
+
+def blink_once(stone: int) -> list[int]:
+    if stone == 0:
+        return [1]
+    elif (stone_len := len(str(stone))) % 2 == 0:
+        return [ 
+            int(str(stone)[: int(stone_len / 2)]), 
+            int(str(stone)[int(stone_len / 2) :])
+        ]
+    else:
+        return [stone * 2024]
