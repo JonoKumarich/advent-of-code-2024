d3a13e78303e69b73d0e5a3b7dfe4419869a5790
Jonathan Kumarich <jonathankumarich@gmail.com>
2024-12-12 16:54:36 +1300

    refactor: slide tidy up

--- src/aoc/day_11.py
+++ src/aoc/day_11.py
@@ -4,44 +4,27 @@ from typing import TextIO
 
 
 def part_01(input: TextIO) -> int:
-    stones = map(int, input.read().strip().split())
-
-    total = 0
-    for stone in stones:
-        total += blink_stone(stone, 25)
-
-    return total
-
-
-@lru_cache
-def blink_stone(stone: int, n_times: int) -> int:
-    if n_times == 0:
-        return 1
-
-    if stone == 0:
-        return blink_stone(1, n_times - 1)
-    elif (stone_len := len(str(stone))) % 2 == 0:
-        return blink_stone(
-            int(str(stone)[: int(stone_len / 2)]), n_times - 1
-        ) + blink_stone(int(str(stone)[int(stone_len / 2) :]), n_times - 1)
-    else:
-        return blink_stone(stone * 2024, n_times - 1)
-
-
+    stones = list(map(int, input.read().strip().split()))
+    return blink_many(stones, 25)
 
 def part_02(input: TextIO) -> int:
-    stones = Counter(map(int, input.read().strip().split()))
+    stones = list(map(int, input.read().strip().split()))
+    return blink_many(stones, 75)
 
-    for _ in range(75):
+def blink_many(stones: list[int], n: int) -> int:
+    stone_freq = Counter(stones)
+
+    for _ in range(n):
         new = defaultdict(lambda: 0)
-        for stone, n in stones.items():
+        for stone, n in stone_freq.items():
             for s in blink_once(stone):
                 new[s] += n
 
-        stones = new
+        stone_freq = new
 
-    return sum(stones.values())
+    return sum(stone_freq.values())
 
+@lru_cache
 def blink_once(stone: int) -> list[int]:
     if stone == 0:
         return [1]
